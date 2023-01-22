be67db90f91b55610873f28b2b1c4a6fe897f752
Karl Essinger <xkaess22@gmail.com>
2023-01-22 18:36:16 +0100

    Indentation

--- Commands/LinkCommand.cs
+++ Commands/LinkCommand.cs
@@ -1,135 +1,133 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using DSharpPlus.SlashCommands.Attributes;
 
-namespace SponsorBoi.Commands
+namespace SponsorBoi.Commands;
+
+public class LinkCommand : ApplicationCommandModule
 {
-	public class LinkCommand : ApplicationCommandModule
+	[SlashRequireGuild]
+	[SlashCommand("link", "Link your Github account for Sponsor syncing")]
+	internal static async Task ExecuteCommand(InteractionContext command)
 	{
-		[SlashRequireGuild]
-		[SlashCommand("link", "Link your Github account for Sponsor syncing")]
-		internal static async Task ExecuteCommand(InteractionContext command)
+		if (Database.TryGetSponsor(command.Member.Id, out Database.SponsorEntry _))
 		{
-			if (Database.TryGetSponsor(command.Member.Id, out Database.SponsorEntry _))
-			{
-				await command.CreateResponseAsync(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "The Discord account " + command.Member.Mention + " is already linked to a Github account."
-				}, true);
-				return;
-			}
-
-			Github.Account githubAccount = null;
-			List<Github.Issue> issues = await Github.GetIssues();
-			foreach (Github.Issue issue in issues)
-			{
-				if (issue.description.Contains(command.Member.Id.ToString()))
-				{
-					githubAccount = issue.author;
-					break;
-				}
-			}
-
-			if (githubAccount == null)
-			{
-				DiscordInteractionResponseBuilder builder = new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder()
-				{
-					Color = DiscordColor.Cyan,
-					Description = "Click [here](" + Utils.GetIssueURL("Discord ID: " + command.Member.Id) + ")"
-								  + " and click submit in order to verify your account.\n\n"
-								  + "Click the button below when done."
-				});
-				builder.AddComponents(new DiscordButtonComponent(ButtonStyle.Primary, "sponsorboi_linkcommandbutton", "Sync Github account"));
-				await command.CreateResponseAsync(builder.AsEphemeral());
-				return;
-			}
-
-			if (Database.TryGetSponsor(githubAccount.id, out Database.SponsorEntry existingSponsor))
-			{
-				await command.CreateResponseAsync(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "The Github account '" + githubAccount.name + "' is already linked to <@" + existingSponsor.discordID + ">"
-				}, true);
-				return;
-			}
-
-			if (!Database.TryAddSponsor(new Database.SponsorEntry { discordID = command.Member.Id, githubID = githubAccount.id }))
-			{
-				await command.CreateResponseAsync(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "Error occured when writing sponsor to database."
-				}, true);
-				return;
-			}
-
 			await command.CreateResponseAsync(new DiscordEmbedBuilder
 			{
-				Color = DiscordColor.Green,
-				Description = "The Github account '" + githubAccount.name + "' is now linked to " + command.Member.Mention + "\n\n"
-							  + "You may now close or delete the Github issue."
+				Color = DiscordColor.Red,
+				Description = "The Discord account " + command.Member.Mention + " is already linked to a Github account."
 			}, true);
-
-			Utils.SyncUserRoles(command.Guild, command.User);
+			return;
 		}
 
-		internal static async Task OnButtonPressed(DiscordInteraction interaction)
+		Github.Account githubAccount = null;
+		List<Github.Issue> issues = await Github.GetIssues();
+		foreach (Github.Issue issue in issues)
 		{
-			Github.Account githubAccount = null;
-			List<Github.Issue> issues = await Github.GetIssues();
-			foreach (Github.Issue issue in issues)
+			if (issue.description.Contains(command.Member.Id.ToString()))
 			{
-				if (issue.description.Contains(interaction.User.Id.ToString()))
-				{
-					githubAccount = issue.author;
-					break;
-				}
+				githubAccount = issue.author;
+				break;
 			}
+		}
 
-			if (githubAccount == null)
+		if (githubAccount == null)
+		{
+			DiscordInteractionResponseBuilder builder = new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder()
 			{
-				await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "Couldn't find an open Github issue containing your Discord ID."
-				}).AsEphemeral());
-				return;
-			}
+				Color = DiscordColor.Cyan,
+				Description = "Click [here](" + Utils.GetIssueURL("Discord ID: " + command.Member.Id) + ")"
+							  + " and click submit in order to verify your account.\n\n"
+							  + "Click the button below when done."
+			});
+			builder.AddComponents(new DiscordButtonComponent(ButtonStyle.Primary, "sponsorboi_linkcommandbutton", "Sync Github account"));
+			await command.CreateResponseAsync(builder.AsEphemeral());
+			return;
+		}
 
-			if (Database.TryGetSponsor(githubAccount.id, out Database.SponsorEntry existingSponsor))
+		if (Database.TryGetSponsor(githubAccount.id, out Database.SponsorEntry existingSponsor))
+		{
+			await command.CreateResponseAsync(new DiscordEmbedBuilder
 			{
-				await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "The Github account '" + githubAccount.name + "' is already linked to <@" + existingSponsor.discordID + ">"
-				}).AsEphemeral());
-				return;
-			}
+				Color = DiscordColor.Red,
+				Description = "The Github account '" + githubAccount.name + "' is already linked to <@" + existingSponsor.discordID + ">"
+			}, true);
+			return;
+		}
 
-			if (!Database.TryAddSponsor(new Database.SponsorEntry { discordID = interaction.User.Id, githubID = githubAccount.id }))
+		if (!Database.TryAddSponsor(new Database.SponsorEntry { discordID = command.Member.Id, githubID = githubAccount.id }))
+		{
+			await command.CreateResponseAsync(new DiscordEmbedBuilder
 			{
-				await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
-				{
-					Color = DiscordColor.Red,
-					Description = "Error occured when writing sponsor to database."
-				}).AsEphemeral());
-				return;
-			}
+				Color = DiscordColor.Red,
+				Description = "Error occured when writing sponsor to database."
+			}, true);
+			return;
+		}
 
+		await command.CreateResponseAsync(new DiscordEmbedBuilder
+		{
+			Color = DiscordColor.Green,
+			Description = "The Github account '" + githubAccount.name + "' is now linked to " + command.Member.Mention + "\n\n"
+						  + "You may now close or delete the Github issue."
+		}, true);
+
+		Utils.SyncUserRoles(command.Guild, command.User);
+	}
+
+	internal static async Task OnButtonPressed(DiscordInteraction interaction)
+	{
+		Github.Account githubAccount = null;
+		List<Github.Issue> issues = await Github.GetIssues();
+		foreach (Github.Issue issue in issues)
+		{
+			if (issue.description.Contains(interaction.User.Id.ToString()))
+			{
+				githubAccount = issue.author;
+				break;
+			}
+		}
+
+		if (githubAccount == null)
+		{
 			await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
 			{
-				Color = DiscordColor.Green,
-				Description = "The Github account '" + githubAccount.name + "' is now linked to " + interaction.User.Mention + "\n\n"
-							  + "You may now close or delete the Github issue."
+				Color = DiscordColor.Red,
+				Description = "Couldn't find an open Github issue containing your Discord ID."
 			}).AsEphemeral());
-
-			Utils.SyncUserRoles(interaction.Guild, interaction.User);
+			return;
 		}
+
+		if (Database.TryGetSponsor(githubAccount.id, out Database.SponsorEntry existingSponsor))
+		{
+			await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
+			{
+				Color = DiscordColor.Red,
+				Description = "The Github account '" + githubAccount.name + "' is already linked to <@" + existingSponsor.discordID + ">"
+			}).AsEphemeral());
+			return;
+		}
+
+		if (!Database.TryAddSponsor(new Database.SponsorEntry { discordID = interaction.User.Id, githubID = githubAccount.id }))
+		{
+			await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
+			{
+				Color = DiscordColor.Red,
+				Description = "Error occured when writing sponsor to database."
+			}).AsEphemeral());
+			return;
+		}
+
+		await interaction.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource,new DiscordInteractionResponseBuilder().AddEmbed(new DiscordEmbedBuilder
+		{
+			Color = DiscordColor.Green,
+			Description = "The Github account '" + githubAccount.name + "' is now linked to " + interaction.User.Mention + "\n\n"
+						  + "You may now close or delete the Github issue."
+		}).AsEphemeral());
+
+		Utils.SyncUserRoles(interaction.Guild, interaction.User);
 	}
 }
